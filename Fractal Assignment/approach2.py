b1dd8166160e013f63da1586be827920f485dd37
garg10may <garg10may@gmail.com>
2023-06-29 13:38:08 +0530

    chnge VectorDBQA to RetrievalQA

--- Fractal Assignment/approach2.py
+++ Fractal Assignment/approach2.py
@@ -2,13 +2,13 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
+from langchain.chains import RetrievalQA
 from langchain.document_loaders import DirectoryLoader
 import openai
 import os
 from dotenv import find_dotenv, load_dotenv
 
-_ = load_dotenv(find_dotenv())  # read local .env file
-# openai.api_key = os.getenv("key")
+_ = load_dotenv(find_dotenv()) 
 key = os.getenv('key')
 
 os.environ['OPENAI_API_KEY'] = key
@@ -20,15 +20,20 @@ text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=100)
 texts = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings(openai_api_key=key)
-docsearch = Chroma.from_documents(texts, embeddings)
+vectordb = Chroma.from_documents(texts, embeddings)
 
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=docsearch,
-                                return_source_documents=True)
+#VectorDBQA is going to be deprecated soon
+# chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb,
+                                # return_source_documents=True)
+
+chain = RetrievalQA.from_chain_type(llm=OpenAI(),
+                                    chain_type='stuff',
+                                    retriever=vectordb.as_retriever())
 
 query = 'In which NAIC member state model adoption is still pending?'
-# answer = qa.run(query)
-# print(answer)
+answer = chain.run(query)
+print(answer)
 
-result = qa({'query': query})
-print(result)
+# result = chain({'query': query})
+# print(result)
 
