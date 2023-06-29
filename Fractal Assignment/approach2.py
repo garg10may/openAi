e500d74d310622bf4940e2c1f9386a41cb062631
garg10may <garg10may@gmail.com>
2023-06-29 14:28:52 +0530

    working with filtered vectordb to save costs

--- Fractal Assignment/approach2.py
+++ Fractal Assignment/approach2.py
@@ -8,32 +8,57 @@ import openai
 import os
 from dotenv import find_dotenv, load_dotenv
 
-_ = load_dotenv(find_dotenv()) 
-key = os.getenv('key')
+def load_key():
+  _ = load_dotenv(find_dotenv()) 
+  key = os.getenv('key')
+  os.environ['OPENAI_API_KEY'] = key
 
-os.environ['OPENAI_API_KEY'] = key
+def split_documents():
+  loader = DirectoryLoader('./', glob='*.pdf')
+  documents = loader.load()
+  text_splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
+  texts = text_splitter.split_documents(documents)
+  return texts
 
-loader = DirectoryLoader('./', glob='*.pdf')
-documents = loader.load()
+def embeddings_model():
+  return OpenAIEmbeddings()
 
-text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=100)
-texts = text_splitter.split_documents(documents)
+def generate_vectors(embeddings_model, texts):
+  vectordb = Chroma.from_documents(texts, embeddings_model)
+  return vectordb
 
-embeddings = OpenAIEmbeddings(openai_api_key=key)
-vectordb = Chroma.from_documents(texts, embeddings)
+def query_vectors1(vectordb, query):
+  #VectorDBQA is going to be deprecated soon
+  chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb)
+  answer = chain.run(query)
+  return answer
 
-#VectorDBQA is going to be deprecated soon
-# chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb,
-                                # return_source_documents=True)
-
-chain = RetrievalQA.from_chain_type(llm=OpenAI(),
+def query_vectors(vectordb, query):
+  chain = RetrievalQA.from_chain_type(llm=OpenAI(),
                                     chain_type='stuff',
                                     retriever=vectordb.as_retriever())
+  answer = chain.run(query)
+  return answer
 
-query = 'In which NAIC member state model adoption is still pending?'
-answer = chain.run(query)
-print(answer)
+def filter_relevant_vectors(embedding_model, vectordb, query):
+  filtered_docs = vectordb.similarity_search(query)
+  vectordb = Chroma.from_documents(documents=filtered_docs, embedding=embedding_model)
+  return vectordb
 
 # result = chain({'query': query})
 # print(result)
+
+if __name__ == '__main__':
+  key = load_key()
+  texts = split_documents()
+  embeddings_model = embeddings_model()
+  vectordb = generate_vectors(embeddings_model, texts)
+  query = 'Give me the short summary of each section. Add line breaks after each summary'
+  #to save costs, otherwise whole vectordb can also be submitted
+  vectordb = filter_relevant_vectors(embeddings_model, vectordb, query) 
+  answer1 = query_vectors(vectordb, query)
+  answer2 = query_vectors1(vectordb, query)
+  print(answer1)
+  print(answer2)
+
 
