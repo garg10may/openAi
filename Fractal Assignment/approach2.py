537e077f945d96e4287942d5e36b659e218987f7
garg10may <garg10may@gmail.com>
2023-06-29 20:33:49 +0530

    improved performance after using loader_cls PYPDF

--- Fractal Assignment/approach2.py
+++ Fractal Assignment/approach2.py
@@ -1,42 +1,58 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
-from langchain import OpenAI, VectorDBQA
+from langchain import OpenAI, VectorDBQA, LLMChain
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings, HuggingFaceInstructEmbeddings
 import openai
 import os
 from dotenv import find_dotenv, load_dotenv
 
 def load_key():
   _ = load_dotenv(find_dotenv()) 
-  key = os.getenv('key')
-  os.environ['OPENAI_API_KEY'] = key
+
+  openai_key = os.getenv('openai_key')
+  os.environ['OPENAI_API_KEY'] = openai_key
+
+  huggingface_key = os.getenv('huggingface_key')
+  os.environ['HUGGINGFACEHUB_API_TOKEN'] = huggingface_key 
 
 def split_documents():
-  loader = DirectoryLoader('./', glob='*.pdf')
+  loader = DirectoryLoader('./', glob='*.pdf', loader_cls=PyPDFLoader)
   documents = loader.load()
   text_splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
   texts = text_splitter.split_documents(documents)
   return texts
 
-def embeddings_model():
-  return OpenAIEmbeddings()
+def embeddings_model(type):
 
-def generate_vectors(embeddings_model, texts):
-  vectordb = Chroma.from_documents(texts, embeddings_model)
+  if type == 'openai':
+    embeddings_model = OpenAIEmbeddings()
+
+  if type == 'huggingface':
+    model_name = 'sentence-transformers/all-mpnet-base-v2'
+    embeddings_model = HuggingFaceEmbeddings(model_name=model_name)
+  
+  return embeddings_model
+
+def generate_vectors(embeddings_model, texts, use_pregenerated_embeddings=False):
+  persist_dir = 'MyTextEmbedding'
+  if use_pregenerated_embeddings: #generate new embeddings
+    vectordb = Chroma(persist_directory=persist_dir, embedding_function=embeddings_model)
+  else:
+    vectordb = Chroma.from_documents(texts, embeddings_model, persist_directory=persist_dir)
+    vectordb.persist()
   return vectordb
 
-def query_vectors1(vectordb, query):
-  #VectorDBQA is going to be deprecated soon
-  chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb)
+def query_vectors(vectordb, query):
+  chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=vectordb.as_retriever())
   answer = chain.run(query)
   return answer
 
-def query_vectors(vectordb, query):
-  chain = RetrievalQA.from_chain_type(llm=OpenAI(),
-                                    chain_type='stuff',
-                                    retriever=vectordb.as_retriever())
+def query_vectors_alternate(vectordb, query):
+  #VectorDBQA is going to be deprecated soon
+  chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb)
   answer = chain.run(query)
   return answer
 
@@ -49,16 +65,20 @@ def filter_relevant_vectors(embedding_model, vectordb, query):
 # print(result)
 
 if __name__ == '__main__':
-  key = load_key()
+  load_key()
   texts = split_documents()
-  embeddings_model = embeddings_model()
+  embeddings_model = embeddings_model('openai')
   vectordb = generate_vectors(embeddings_model, texts)
   query = 'Give me the short summary of each section. Add line breaks after each summary'
+  query = 'Give me summary of what this document is all about? How many different sections are there? give me brief summary of each of the section explaining what it highlights the most?'
+  query = 'Which NAIC member states have still not implemented the model and which has implemented the same or there are some states that have done it partially?'
+  # query = 'Exlain in simple words what is this PDF about?' 
   #to save costs, otherwise whole vectordb can also be submitted
-  vectordb = filter_relevant_vectors(embeddings_model, vectordb, query) 
+  # vectordb = filter_relevant_vectors(embeddings_model, vectordb, query) 
   answer1 = query_vectors(vectordb, query)
-  answer2 = query_vectors1(vectordb, query)
   print(answer1)
-  print(answer2)
+
+  # answer2 = query_vectors_alternate(vectordb, query) #more of less same type of answer, that is both would hallucinate, or both won't, or both would say don't know. sometimes only difference would be in wordings
+  # print(answer2)
 
 
