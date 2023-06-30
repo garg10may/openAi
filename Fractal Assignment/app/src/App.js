3c4a12a68269b04bf54e94dd7f98d54d3828e0ed
garg10may <garg10may@gmail.com>
2023-06-30 14:45:24 +0530

    added frontend

---
+++ Fractal Assignment/app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from './chat';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          <Chat></Chat>
+        </p>
+    </div>
+  );
+}
+
+export default App;
